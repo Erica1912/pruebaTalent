1a696453fcea14fcb579fb150be578cfc2c91b65
Erica1912 <eri.ruiz@hotmail.com.ar>
2021-02-04 23:04:34 -0500

    Delete etapaII.js

--- js/etapaII.js
+++
@@ -1,9 +0,0 @@
-{
-    jobs: [
-      ...document.querySelectorAll(".job-search-result"),
-    ].map(({ children }) => ({
-      title: children[0].querySelector("h3").innerText,
-      location: children[0].querySelector("p").innerText,
-      url: children[1].querySelector("a").href,
-    }));
-  }
