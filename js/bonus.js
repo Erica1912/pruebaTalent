eaf3d46ddb6ef8c5c323c8294a0387d766685a08
Erica1912 <eri.ruiz@hotmail.com.ar>
2021-02-04 23:05:54 -0500

    Entrega Final

--- js/bonus.js
+++ js/bonus.js
@@ -1,10 +1,7 @@
-
-var nodo= document.querySelector (".job-search-result");
-for (var i=0; i<nodo.length; i++) {
-    querySelector("h3").innerText       // Ejecutar acciones sobre el nodo listaNodosOperadores[i].
-       }
-
-
-document.querySelectorAll(".job-search-result").map(({ children }) => ({
-        console.log("entra")
-       }));
+console.table(
+    [...document.querySelectorAll(".job-search-result")].map(({ children }) => ({
+      Job_Title: children[0].querySelector("h3").innerText,
+      Location: children[0].querySelector("p").innerText,
+      Job_Url: children[1].querySelector("a").href,
+    }))
+  );
